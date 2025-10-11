55530ef5a6182e18835cea6eb4c4258cd1087932
Dave Blaschke <deblaschke@yahoo.com>
2025-10-11 11:10:36 -0500

    Add compilations, async image load

--- js/slideshow.js
+++ js/slideshow.js
@@ -8,6 +8,8 @@ var SLIDESHOW_AUDIO = false;
 var SLIDESHOW_INDEX_FROM = -1;
 // Indicates ending slide (-1 indicates none)
 var SLIDESHOW_INDEX_TO = -1;
+// Indicates number of slides to preload before initiating slideshow (remainder loaded asynchronously)
+var SLIDESHOW_PRELOAD = 10;
 
 // Current slide index
 var slideIndex;
@@ -50,6 +52,156 @@ if ("URLSearchParams" in window) {
   if (SLIDESHOW_INDEX_FROM > -1 || SLIDESHOW_INDEX_TO > -1) {
     slideshowFromTo = true;
   }
+}
+
+// getDescription returns description for compilation slide including trip name
+function getDescription(path) {
+  var result = "";
+
+  // path is of format "*/dir/file.jpg"
+  // dir is of format "nnnn[-nn]*"
+  // file is of format "nnn_picture-description"
+
+  // Process valid paths (must have directory separator and .jpg extension)
+  path = decodeURI(path);
+  var index = path.lastIndexOf('/');
+  if (index >= 0 && path.lastIndexOf('.jpg') > index) {
+    var file = path.substring(index + 1, path.lastIndexOf('.'));
+
+    // File begins with "nnn_" for slides
+    if (/^[0-9]{3}_/.test(file)) {
+      if (/^[0-9]{3}_[C-Z]{1}[0-9]{7}/.test(file)) {
+        // Found digital camera picture name ("nnn_Annnnnnn")
+        result = file.substring(4, 12);
+      } else if (/^[0-9]{3}_[A-B]{1}[0-9]{3}-[0-9]{2}/.test(file)) {
+        // Found film camera picture name ("nnn_Annn-nn")
+        result = file.substring(4, 11);
+      } else if (/^[0-9]{3}_[0-9]{8}T[0-9]{6}/.test(file)) {
+        // Found cell phone camera picture name ("nnn_nnnnnnnnTnnnnnn")
+        result = file.substring(4, 12) + file.substring(13, 19);
+      }
+
+      // Continue processing if recognized picture name
+      if (result.length > 0) {
+        // Handle description if present
+        index = file.indexOf('-', result.length + 4);
+        if (index >= 0) {
+          // Picture description is everything after "-"
+          var desc = file.substring(index + 1);
+
+          // Replace underscores with spaces
+          result = result + " - " + desc.replace(/_/g, ' ');
+
+          // Replace special characters ("[*]") with HTML entity names ("&*;")
+          index = file.indexOf('[');
+          if (index >= 0 && index < file.indexOf(']')) {
+            result = result.replace(/\[/g, '&');
+            result = result.replace(/\]/g, ';');
+          }
+        }
+      }
+    }
+
+    // Append trip name if valid dir format
+    var dir = path.match(/\/[0-9]{4}(-[0-9]{2})?[A-Z]{1}[A-Z0-9]*\//gi);
+    if (result.length > 0 && dir != null) {
+      var trip = "";
+
+      // Get trip name from dir
+      var tripDir = dir[0].slice(1, -1);
+      if (["2017Dusseldorf", "2018Dusseldorf"].includes(tripDir)) {
+        trip = "Germany compilation";
+      } else if (all_trips.has(tripDir)) {
+        trip = all_trips.get(tripDir).title;
+      }
+
+      // Append trip name if one exists
+      if (trip.length > 0) {
+        result = result + "<BR>(" + trip + ")";
+      }
+    }
+  }
+
+  // Display space to occupy slideName span if description empty
+  if (result.length == 0) {
+    result = "&nbsp;";
+  }
+
+  return result;
+}
+
+// SPECIAL CASE: GitHub
+function getRegion(dir) {
+  var region = "";
+  if (all_trips.has(dir)) {
+    region = "https://deblaschke.github.io/travel-slideshows-" + all_trips.get(dir).region;
+  }
+  return region;
+}
+
+// preloadSlideshow adds title and preloaded slides to slideshow
+function preloadSlideshow(targetElement, titleSlide, filtered_slides) {
+  // Add title to slideshow
+  var imgElem = document.createElement('img');
+  imgElem.src = titleSlide == null ? "QUERY/title.jpg" : titleSlide;
+  imgElem.alt = "Title";
+  imgElem.className = "tripPix";
+  imgElem.style = "width:95%;height:71%;";
+  targetElement.appendChild(imgElem);
+
+  // Add initial matching slides to slideshow
+  var numPreload = filtered_slides.length > SLIDESHOW_PRELOAD ? SLIDESHOW_PRELOAD : filtered_slides.length;
+  for (var i = 0; i < numPreload; i++) {
+    var slide = filtered_slides[i];
+    imgElem = document.createElement('img');
+    imgElem.src = getRegion(slide.dir) + "/" + slide.dir + "/" + slide.file + ".jpg";
+    imgElem.alt = 'Slide';
+    // DEBUG: Uncomment following line along with 'document.getElementById("slideName").innerHTML =' lines below
+    // imgElem.deb_attrs = slide.attrs;
+    imgElem.className = 'tripPix';
+    imgElem.style = slide.style;
+    targetElement.appendChild(imgElem);
+  }
+}
+
+// loadSlideshow asynchronously adds non-preloaded slides and credits to slideshow
+async function loadSlideshow(targetElement, filtered_slides) {
+    var imgElem;
+
+    // Add matching slides to slideshow, if any
+    for (var i = SLIDESHOW_PRELOAD; i < filtered_slides.length; i++) {
+      var slide = filtered_slides[i];
+      imgElem = document.createElement('img');
+      imgElem.src = getRegion(slide.dir) + "/" + slide.dir + "/" + slide.file + ".jpg";
+      imgElem.alt = 'Slide';
+      // DEBUG: Uncomment following line along with 'document.getElementById("slideName").innerHTML =' lines below
+      // imgElem.deb_attrs = slide.attrs;
+      imgElem.className = 'tripPix';
+      imgElem.style = slide.style;
+      targetElement.appendChild(imgElem);
+    }
+
+    // Add credits to slideshow
+    imgElem = document.createElement('img');
+    imgElem.src = "images/theend1.jpg";
+    imgElem.alt = "Credit";
+    imgElem.className = "tripPix";
+    imgElem.style = "width:95%;height:71%;";
+    targetElement.appendChild(imgElem);
+    imgElem = document.createElement('img');
+    imgElem.src = "images/theend2.jpg";
+    imgElem.alt = "Credit";
+    imgElem.className = "tripPix";
+    imgElem.style = "width:95%;height:71%;";
+    targetElement.appendChild(imgElem);
+    if (SLIDESHOW_AUDIO) {
+      imgElem = document.createElement('img');
+      imgElem.src = "images/theend3.jpg";
+      imgElem.alt = "Credit";
+      imgElem.className = "tripPix";
+      imgElem.style = "width:95%;height:71%;";
+      targetElement.appendChild(imgElem);
+    }
 }
 
 // reduceSlideshow removes elements from slideshowElems that are outside specified from/to range
@@ -160,8 +312,11 @@ function showPic(n) {
   slideshowElems[slideIndex-1].style.display = "block";
 
   // Set slide description
-  document.getElementById("slideName").innerHTML = getDescription(slideshowElems[slideIndex-1].src); // PRODUCTION
-  // document.getElementById("slideName").innerHTML = getDescription(slideshowElems[slideIndex-1].src) + "<BR>[" + (slideshowElems[slideIndex-1].deb_attrs || '') + "]"; // DEBUG
+  document.getElementById("slideName").innerHTML = getDescription(slideshowElems[slideIndex-1].src);
+  // DEBUG: Uncomment following lines along with 'imgElem.deb_attrs = slide.attrs;' lines above
+  // document.getElementById("slideName").innerHTML = (slideshowElems[slideIndex-1].alt == 'Slide')
+  //   ? getDescription(slideshowElems[slideIndex-1].src) + '<br><font size="-1" color="blue">[' + (slideshowElems[slideIndex-1].deb_attrs || '') + '] (#' + (slideIndex-1) + '/' + (slideshowElems.length-4) + ')</font>'
+  //   : "";
 }
 
 // slideshow runs automatic slideshow
@@ -258,15 +413,4 @@ window.onresize = function() {
 document.addEventListener("DOMContentLoaded", (event) => {
   isMobileDevice = /iPhone|Android|BlackBerry/i.test(navigator.userAgent);
   setPicDimensions();
-  slideIndex = 0;
-
-  // Initiate slideshow if not search page
-  if (!window.location.pathname.includes("SEARCH")) {
-    if (MANUAL_SLIDESHOW) {
-      hidePlayButton();
-      showPic(1);
-    } else {
-      slideshow();
-    }
-  }
 });
